730f4f79b28a966c87bacd0d4a1941818cd3f037
enthus-io <lfojedaguzman@gmail.com>
2021-06-17 18:37:44 -0500

    Completed sillySentence Challenge

--- 3. JavaScript Syntax, Part I/Code Challenges JavaScript Fundamentals/sillySentence Challenge/main.js
+++ 3. JavaScript Syntax, Part I/Code Challenges JavaScript Fundamentals/sillySentence Challenge/main.js
@@ -1,14 +1,13 @@
 // Write your function here:
-function numImaginaryFriends(friends) {
-  return Math.ceil(friends / 4);
+function sillySentence(verb1, verb2, noun) {
+  let s = `I am so ${verb1} because I ${verb2} coding! Time to write some more awesome ${noun}!`;
+
+  return s;
 }
 
 
 
-
-
-// Uncomment the lines below when you're ready to try out your function
-console.log(numImaginaryFriends(19)) // Should print 5
-console.log(numImaginaryFriends(10)) // Should print 3 (2.5 rounded up!)
+// Uncomment the line below when you're ready to try out your function
+console.log(sillySentence('excited', 'love', 'functions'))
 
 // We encourage you to add more function calls of your own to test your code!
