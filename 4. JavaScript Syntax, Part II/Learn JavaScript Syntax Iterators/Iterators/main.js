b76b63bb5f58fc40c3900ee84410854e9988b61e
enthus-io <lfojedaguzman@gmail.com>
2021-06-28 04:13:03 -0500

    Working on Iterators

---
+++ 4. JavaScript Syntax, Part II/Learn JavaScript Syntax Iterators/Iterators/main.js
@@ -0,0 +1,67 @@
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+    console.log(artist + ' is one of my favorite artists.');
+});
+artists.forEach(function (artist) {
+    console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+    return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+    return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function (it) {
+    console.log('I want to eat a ' + it);
+})
+
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+
+
+
+
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(function(item) {
+  return item[0];
+});
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(function(item){
+  return item / 100;
+})
+
+console.log(smallNumbers);
